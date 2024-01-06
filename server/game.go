291cffeb7894651aea31bdd23f1bfae58657ec3e
Julius S <jluuiss@gmail.com>
2024-01-06 23:59:37 +0800

    break

--- server/game.go
+++ server/game.go
@@ -1,7 +1,31 @@
 package main
 
 import (
+	"errors"
 	"math/rand"
+)
+
+const (
+	PhaseThaler1 uint8 = iota
+	PhaseThaler2 uint8 = iota
+	PhasePlayer1 uint8 = iota
+	PhasePlayer2 uint8 = iota
+)
+
+const (
+	MovePlayer1 uint8 = iota
+	MovePlayer2 uint8 = iota
+)
+
+const (
+	ColorBlue uint8 = iota
+	ColorGreen uint8 = iota
+	ColorOrange uint8 = iota
+	ColorPink uint8 = iota
+	ColorRed uint8 = iota
+	ColorWhite uint8 = iota
+	ColorYellow uint8 = iota
+	ColorUninitialized uint8 = iota
 )
 
 type Coord struct {
@@ -13,7 +37,9 @@ type Game struct {
 	Pegs [7]Coord
 	Thaler Coord
 	CurrentTurn uint8
-	PlayerChoices [2]uint8
+	Player1ColorChoice uint8
+	Player2ColorChoice uint8
+	Phase uint8
 }
 
 func generate7RandomCoordinates(r *rand.Rand) [7]Coord {
@@ -27,8 +53,16 @@ func generate7RandomCoordinates(r *rand.Rand) [7]Coord {
 	return res
 }
 
-func InitializeGame(r *rand.Rand) Game {
-	game := Game{}
-	game.Pegs = generate7RandomCoordinates(r)
-	return game
+func InitializeGame(r *rand.Rand, p uint8) (Game, error) {
+	if p != PhaseThaler1 || p != PhaseThaler2 {
+		return Game{},
+			errors.New("InitializeGame: expected PhaseThaler1 or PhaseThaler2 but got something else")
+	}
+
+	return Game{
+		Pegs: generate7RandomCoordinates(r),
+		Player1ColorChoice: ColorUninitialized,
+		Player2ColorChoice: ColorUninitialized,
+	},
+		nil
 }
